828a72a1e1ed1aabbc2f74d398b6944bc537b278
ADITYA_SADEWALE <adityasadewale@gmail.com>
2026-09-14 22:54:13 +0530

    Update Auth.tsx

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -13,6 +13,7 @@ export function Auth() {
   const [error, setError] = useState('');
   const [loading, setLoading] = useState(false);
 
+  
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     setError('');
